e6de2ba5db91a841ef941353b16a8fe6451eca59
Drew Barrett <dfb1998@gmail.com>
2015-09-29 08:41:34 -0400

    talent started
    Signed-off-by: Drew Barrett <dfb1998@gmail.com>

--- talents/main.cpp
+++ talents/main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
+//Drew Barrett
+string input1;
+string input2;
+
+int parseInput(string inputfile){
+    input1 = "";
+    input2 = "";
+    ifstream infile(inputfile.c_str());
+    string temp;
+    while(!infile.eof()){
+        temp = "";
+        getline(infile,temp);
+        if(input1 == ""){
+            input1 = temp;
+        }else if (input2 == ""){
+            input2 = temp;
+        }else{
+            continue;
+        }
+    }
+    cout << inputfile << " input: \n" << input1 << " and " << input2 << endl;
+    infile.close();
+    return 0;
+}
+
+int output(){
+
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    parseInput("talent1.txt");
+    output();
     return 0;
 }
