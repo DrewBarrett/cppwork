69ead5d9ecb0d896cfb36f8749dd1b53ae752bb1
Drew Barrett <dfb1998@gmail.com>
2015-11-13 11:04:17 -0500

    updated cards
    Signed-off-by: Drew Barrett <dfb1998@gmail.com>

--- cards/main.cpp
+++ cards/main.cpp
@@ -24,17 +24,18 @@ int main()
     ALLEGRO_BITMAP *bmp = al_create_bitmap(1280, 640);
     Card a('c', "Ace", 14, "c1.bmp");
     //while (!key[KEY_ESC])
-    {
+
         //Lets view the deck first in order:
 
-        for (int i = 0; i < 13; i++) {
-            for (int j = 0; j < 4; j++) {
-                //blit(game.deal().getImg(), bmp, 0, 0, x + i * 75, y + j * 100, 71, 96);
-                al_set_target_bitmap(bmp);
-                al_draw_bitmap(game.deal().getImg(),x + i * 75, y + j * 100,0);
-                //textout_ex(bmp, font, game.deal().getFileDir().c_str(),x + j * 12 , y + i*100, -1, 0);
-            }
+    for (int i = 0; i < 13; i++) {
+        for (int j = 0; j < 4; j++) {
+            //blit(game.deal().getImg(), bmp, 0, 0, x + i * 75, y + j * 100, 71, 96);
+            al_set_target_bitmap(bmp);
+            game.shuffleDeck();
+            //al_draw_bitmap(game.deal().getImg(),x + i * 75, y + j * 100,0);
+            //textout_ex(bmp, font, game.deal().getFileDir().c_str(),x + j * 12 , y + i*100, -1, 0);
         }
+    }
     queue = al_create_event_queue();
 	al_register_event_source(queue, al_get_keyboard_event_source());
 	al_register_event_source(queue, al_get_mouse_event_source());
@@ -42,34 +43,41 @@ int main()
 	al_register_event_source(queue, al_get_timer_event_source(timer));
 	al_start_timer(timer);
 	bool redraw = true;
-        while (1) {
-            ALLEGRO_EVENT event;
-            al_wait_for_event(queue, &event);
-            //al_get_next_event(queue, &event);
+    while (1) {
+        ALLEGRO_EVENT event;
+        al_wait_for_event(queue, &event);
+        //al_get_next_event(queue, &event);
 
-            if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+        if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+            break;
+        if (event.type == ALLEGRO_EVENT_KEY_DOWN) {
+            if (event.keyboard.keycode == ALLEGRO_KEY_ESCAPE)
                 break;
-            if (event.type == ALLEGRO_EVENT_KEY_DOWN) {
-                if (event.keyboard.keycode == ALLEGRO_KEY_ESCAPE)
-                    break;
-                if (event.keyboard.keycode == ALLEGRO_KEY_RIGHT)
-                    bx+= 20;
-                if (event.keyboard.keycode == ALLEGRO_KEY_LEFT){
-                    bx-= 20;
-                }
+            if (event.keyboard.keycode == ALLEGRO_KEY_RIGHT)
+                bx+= 20;
+            if (event.keyboard.keycode == ALLEGRO_KEY_LEFT){
+                bx-= 20;
             }
-            if (event.type == ALLEGRO_EVENT_TIMER) {
-                redraw = true;
+            game.deal();
+        }
+        if (event.type == ALLEGRO_EVENT_TIMER) {
+            redraw = true;
+        }
+        if (redraw && al_is_event_queue_empty(queue)) {
+            al_set_target_bitmap(al_get_backbuffer(display));
+            al_clear_to_color(al_map_rgb(0, 0, 0));
+            //al_draw_bitmap(bmp, bx, 0, 0);
+            Card c;
+            for (int i = 0; i<game.size() * 2; i+=2){
+                al_draw_bitmap(c.getImg(),i,i,0);
             }
-            if (redraw && al_is_event_queue_empty(queue)) {
-                al_set_target_bitmap(al_get_backbuffer(display));
-                al_clear_to_color(al_map_rgb(0, 0, 0));
-                al_draw_bitmap(bmp, bx, 0, 0);
-                al_flip_display();
-                redraw = false;
-            }
+            //game.deal();
+            //al_draw_bitmap(c.getImg(),30,30,0);
+            al_flip_display();
+            redraw = false;
         }
     }
+
     al_destroy_display(display);
     return 0;
 }
