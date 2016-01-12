17fe61a8478a55f3f4fa0094b7cad3783a303db4
Drew Barrett <dfb1998@gmail.com>
2016-01-12 08:34:01 -0500

    something something grid

---
+++ battleship/ship.cpp
@@ -0,0 +1,9 @@
+#include "ship.h"
+
+ship::ship(int ss, int x, int y, int deg)
+{
+    ssize = ss;
+    xPos = x;
+    yPos = y;
+    rotation = deg;
+}
---
+++ battleship/ship.h
@@ -0,0 +1,50 @@
+#ifndef SHIP_H
+#define SHIP_H
+
+
+class ship
+{
+    public:
+        /** Default constructor */
+        ship(int ss, int x, int y, int deg);
+        /** Access ssize
+         * \return The current value of ssize
+         */
+        int Getssize() { return ssize; }
+        /** Set ssize
+         * \param val New value to set
+         */
+        void Setssize(int val) { ssize = val; }
+        /** Access xPos
+         * \return The current value of xPos
+         */
+        int GetxPos() { return xPos; }
+        /** Set xPos
+         * \param val New value to set
+         */
+        void SetxPos(int val) { xPos = val; }
+        /** Access yPos
+         * \return The current value of yPos
+         */
+        int GetyPos() { return yPos; }
+        /** Set yPos
+         * \param val New value to set
+         */
+        void SetyPos(int val) { yPos = val; }
+        /** Access rotation
+         * \return The current value of rotation
+         */
+        int Getrotation() { return rotation; }
+        /** Set rotation
+         * \param val New value to set
+         */
+        void Setrotation(int val) { rotation = val; }
+    protected:
+    private:
+        int ssize; //!< Member variable "ssize"
+        int xPos; //!< Member variable "xPos"
+        int yPos; //!< Member variable "yPos"
+        int rotation; //!< Member variable "rotation"
+};
+
+#endif // SHIP_H
